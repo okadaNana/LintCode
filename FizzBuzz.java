a181d7bc72090aa3af412c2fb99084088cdc97bc
Owen <xiaotu.dev@gmail.com>
2016-04-18 16:54:51 +0800

    Fizz Buzz

---
+++ FizzBuzz.java
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * param n: As description.
+     * return: A list of strings.
+     */
+    public ArrayList<String> fizzBuzz(int n) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("fizz buzz");
+            } else if (i % 3 == 0) {
+                list.add("fizz");
+            } else if (i % 5 == 0) {
+                list.add("buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+}
