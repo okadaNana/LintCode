3594217d29d35503c296d49d1edfc256c3808baa
Owen <xiaotu.dev@gmail.com>
2016-04-19 00:21:42 +0800

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    private volatile static Solution instance;
+    
+    /**
+     * @return: The same instance of this class every time
+     */
+    public static Solution getInstance() {
+        if (null == instance) {
+            synchronized (Solution.class) {
+                if (null == instance) {
+                    instance = new Solution();
+                }
+            }
+        }
+        
+        return instance;
+    }
+};
