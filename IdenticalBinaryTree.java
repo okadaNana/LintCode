2f176aeb3e1150583f1c8b210de7cf1e0fb6db71
Owen <xiaotu.dev@gmail.com>
2016-04-18 23:09:34 +0800

    Identical Binary Tree

---
+++ IdenticalBinaryTree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param a, b, the root of binary trees.
+     * @return true if they are identical, or false.
+     */
+    public boolean isIdentical(TreeNode a, TreeNode b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        
+        if (a == null || b == null) {
+            return false;
+        }
+        
+        if (a.val != b.val) {
+            return false;
+        }
+        
+        if (!isIdentical(a.left, b.left)) {
+            return false;
+        }
+        
+        if (!isIdentical(a.right, b.right)) {
+            return false;
+        }
+        
+        return true;
+    }
+}
