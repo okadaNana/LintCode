757da974cf00f1867694d46e14ded0aff8bd4750
Owen <xiaotu.dev@gmail.com>
2016-04-18 17:05:03 +0800

    Single Number

---
+++ SingleNumber.java
@@ -0,0 +1,13 @@
+public class Solution {
+    /**
+      *@param A : an integer array
+      *return : a integer 
+      */
+    public int singleNumber(int[] A) {
+        int result = 0;
+        for (int i = 0; i < A.length; i++) {
+            result ^= A[i];
+        }
+        return result;
+    }
+}
