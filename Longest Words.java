bbc660387bd6993a6b07ad251985e0f44fe908cf
Owen <xiaotu.dev@gmail.com>
2016-04-22 21:52:38 +0800

    Longest Words v2

--- Longest Words.java
+++ Longest Words.java
@@ -5,17 +5,18 @@ class Solution {
      */
     ArrayList<String> longestWords(String[] dictionary) {
         int maxLen = 0;
-        for (int i = 0; i < dictionary.length; i++) {
-            if (dictionary[i].length() > maxLen) {
-                maxLen = dictionary[i].length();
-            }
-        }
         ArrayList<String> result = new ArrayList<>();
+        
         for (int i = 0; i < dictionary.length; i++) {
             if (dictionary[i].length() == maxLen) {
                 result.add(dictionary[i]);
+            } else if (dictionary[i].length() > maxLen) {
+                result.clear();
+                maxLen = dictionary[i].length();
+                result.add(dictionary[i]);
             }
         }
+        
         return result;
     }
 };
