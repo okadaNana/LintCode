2c04d0add359289d24a48f2d5f09b89ba7a12143
Owen <xiaotu.dev@gmail.com>
2016-07-28 10:39:59 +0800

    Intersection of Two Arrays II

---
+++ Intersection of Two Arrays II.java
@@ -0,0 +1,35 @@
+public class Solution {
+    /**
+     * @param nums1 an integer array
+     * @param nums2 an integer array
+     * @return an integer array
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.containsKey(nums1[i])) {
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            } else {
+                map.put(nums1[i], 1);
+            }
+        }
+        
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+                resultList.add(nums2[i]);
+            }
+        }
+        
+        int[] resultArray = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            resultArray[i] = resultList.get(i);
+        }
+        return resultArray;
+    }
+}
